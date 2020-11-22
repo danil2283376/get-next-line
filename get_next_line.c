c905466b78f457b94122eeb8d1353ea077dfdfec
Spawn Colen <scolen@pr-l2.kzn.21-school.ru>
2020-11-22 14:57:05 +0300

    endGNL

--- get_next_line.c
+++ get_next_line.c
@@ -5,78 +5,82 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: scolen <scolen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/18 21:12:56 by scolen            #+#    #+#             */
-/*   Updated: 2020/11/18 21:26:26 by scolen           ###   ########.fr       */
+/*   Created: 2020/11/18 21:42:09 by scolen            #+#    #+#             */
+/*   Updated: 2020/11/22 14:40:28 by scolen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <fcntl.h>
 
 static char		*gnl_remainder(char **remainder, char **line)
 {
 	char *p_n;
+	char *leaks;
 
 	p_n = NULL;
 	if (*remainder)
 		if ((p_n = ft_strchr(*remainder, '\n')))
 		{
-			*p_n++ = '\0';
+			*p_n = '\0';
 			*line = ft_strdup(*remainder);
-			if (*p_n)
-				ft_strcpy(*remainder, p_n);
-			else
-			{
-				free(*remainder);
-				*remainder = NULL;
-			}
+			leaks = *remainder;
+			*remainder = ft_strdup(++p_n);
+			free(leaks);
 		}
 		else
 		{
 			*line = ft_strdup(*remainder);
 			free(*remainder);
-			remainder = NULL;
+			*remainder = NULL;
 		}
 	else
 		*line = ft_strdup("");
 	return (p_n);
 }
 
-static int		return_eof(int r_n, char **remainder, char *p_n, char *buf)
+static int		return_fail_value(char *buf)
 {
-	free(buf);
-	if (r_n == 0)
-		*remainder = ft_strdup("");
-	if (p_n != NULL || r_n)
-		return (1);
-	else
-		return (0);
+	if (buf)
+		free(buf);
+	return (-1);
+}
+
+static char		*clean_ft_strjoin(char *line, char *buff)
+{
+	char	*leaks;
+
+	leaks = line;
+	if (!(line = ft_strjoin(line, buff)))
+		return (NULL);
+	free(leaks);
+	return (line);
 }
 
 int				get_next_line(int fd, char **line)
 {
 	char		*buf;
-	static char *remainder[1024];
+	static char *remainder;
 	char		*p_n;
-	char		*ptr;
 	int			read_next;
 
-	if (fd < 0 || BUFFER_SIZE < 1 || !(buf = malloc(BUFFER_SIZE + 1)))
+	buf = NULL;
+	if (BUFFER_SIZE < 1 || !(buf = malloc(BUFFER_SIZE + 1))
+		|| (read(fd, buf, 0) < 0))
 		return (-1);
-	*line = NULL;
-	p_n = gnl_remainder(&remainder[fd], line);
+	p_n = gnl_remainder(&remainder, line);
 	while (!p_n && (read_next = read(fd, buf, BUFFER_SIZE)))
 	{
-		if (read_next == -1)
-			return (-1);
 		buf[read_next] = '\0';
 		if ((p_n = ft_strchr(buf, '\n')))
 		{
-			p_n[0] = '\0';
-			if (!(remainder[fd] = ft_strdup(++p_n)))
-				return (-1);
+			*p_n = '\0';
+			if (!(remainder = ft_strdup(++p_n)))
+				return (return_fail_value(buf));
 		}
-		ptr = *line;
-		*line = ft_strjoin(ptr, buf);
+		if (!(*line = clean_ft_strjoin(*line, buf)))
+			return (return_fail_value(buf));
 	}
-	return (return_eof(read_next, &remainder[fd], p_n, buf));
+	free(buf);
+	return (p_n != NULL ? 1 : 0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: scolen <scolen@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/08 15:35:21 by scolen            #+#    #+#             */
-/*   Updated: 2020/11/18 21:14:14 by scolen           ###   ########.fr       */
+/*   Created: 2020/11/18 21:42:29 by scolen            #+#    #+#             */
+/*   Updated: 2020/11/22 12:59:30 by scolen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,10 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	char			*ptr;
 	unsigned int	start_ptr;
-	char			*ptr_old;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	start_ptr = 0;
-	ptr_old = s1;
 	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (ptr == NULL)
 		return (NULL);
@@ -40,7 +38,6 @@ char	*ft_strjoin(char *s1, char *s2)
 	while (*s2)
 		ptr[start_ptr++] = *s2++;
 	ptr[start_ptr] = '\0';
-	free(ptr_old);
 	return (ptr);
 }
 
